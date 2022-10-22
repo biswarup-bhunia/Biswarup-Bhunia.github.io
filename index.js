09c3be430d79adb6de34fb9dc78dc8ca3cecdec2
Rup1999 <rup.bhunia1999@gmail.com>
2022-10-23 05:16:28 +0530

    My own website

---
+++ index.js
@@ -0,0 +1,40 @@
+let details=document.getElementsByClassName("skill-ed");
+let content=document.getElementsByClassName("contents");
+
+opentab=(name)=>{
+    for(detail of details){
+        detail.classList.remove("active");
+    }
+    for(con of content){
+        con.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active");
+    document.getElementById(name).classList.add("active-tab");
+}
+
+
+let sidemenu=document.getElementById("sidemenu");
+
+openmenu=()=>{
+sidemenu.style.right="0";
+}
+
+closemenu=()=>{
+    sidemenu.style.right="-200px";
+}
+// Message sending script
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzSRLhYpIkgrJDtYqvXX3iNXSY2ny0dDjliYc4t4_B2wpFzXMM706RnuQhrjyf2vtNn/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg=document.getElementById("msg");
+form.addEventListener('submit', e => {
+e.preventDefault()
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+    msg.innerHTML="Message Sent Successfully";
+    setTimeout(()=>{
+        msg.innerHTML="";
+    },5000);
+    form.reset();
+})
+    .catch(error => console.error('Error!', error.message))
+})
